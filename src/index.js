b9ac12faa9fcc9f3b65b3d70081ca447a5d0ca50
Youssouf Elazizi <youssoufelazizi@gmail.com>
2018-04-14 20:26:26 +0100

    useing story-router as root app

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./Utils/Home";
-import Palette from "./Utils/Palette";
-import WithProp from "./Utils/WithProp";
+import StoryRouter from "story-router";
+import "story-router/story-router.css";
+
+// import stories from the stories folder
 import Stories from "./stories/index";
 
-import "./style/index.scss";
-
-//const Components = Object.entries(require("./stories/*.js"));
-const App = () => (
-  <Router>
-    <div className="main">
-      <Palette />
-      <Route exact path="/" component={Home} />
-      {Stories.map(({ name, component, props }, i) => {
-        const Component = !!props ? WithProp(component)(props) : component;
-        return <Route key={i} exact path={`/${name}`} component={Component} />;
-      })}
-    </div>
-  </Router>
-);
+const App = () => <StoryRouter stories={Stories} />;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
